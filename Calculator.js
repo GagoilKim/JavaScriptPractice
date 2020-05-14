0536a4f8459aa458be612d19f633a2197602980e
Gagoilkim <dkim558@wisc.edu>
2020-05-14 14:58:05 -0500

    First Push

---
+++ Calculator.js
@@ -0,0 +1,12 @@
+'use strict'
+window.addEventListener("load", function(){
+    let txt = this.document.getElementById("input1");
+    let txt2 = this.document.getElementById("input2");
+    let txt3 = this.document.getElementById("input3");
+    let button = this.document.getElementById("button1");
+    
+    button.onclick = function(){
+        console.log('add')
+    };
+
+});
