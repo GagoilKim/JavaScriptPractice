09f639ebdbcda794cd5768a41eb0b45d4c405067
Gagoilkim <dkim558@wisc.edu>
2020-05-15 20:03:48 -0500

    Update JS

--- Calculator.js
+++ Calculator.js
@@ -1,8 +1,42 @@
 'use strict'
 const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation')
-const deleteButtons = document.querySelectorAll('[data-delete')
-const allClearButtons = document.querySelectorAll('[data-all-clear')
-const equalsButtons = document.querySelectorAll('[data-equals')
-const previousButtons = document.querySelectorAll('[data-previous')
-const currentButtons = document.querySelectorAll('[data-current')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const deleteButtons = document.querySelectorAll('[data-delete]')
+const allClearButtons = document.querySelectorAll('[data-all-clear]')
+const equalsButtons = document.querySelectorAll('[data-equals]')
+const previousButtons = document.querySelectorAll('[data-previous]')
+const currentButtons = document.querySelectorAll('[data-current]')
+const Calculator = new Calculator(previousButtonsElement, currentButtonsElement)
+numberButtons.forEach(button => {
+    button.addEventListener("click", ()=>{
+        Calculator.addNumber(button.innerText)
+        Calculator.updateDisplay()
+    })
+});
+
+class Calculator {
+    constructor(previousButtons, currentButtons){
+        this.previousButtons = previousButtons
+        this.currentButtons = currentButtons
+    }
+    clear(){
+        this.currentButtons = ""
+        this.previousButtons = ""
+        this.operation = undefined
+    }
+    delete(){
+
+    }
+    addNumber(number){
+        this.currentButtons = number
+    }
+    operation(operation){
+
+    }
+    compute(){
+
+    }
+    updateDisplay(){
+        this.currentButtonsElement.innerText = this.currentButtons
+    }
+}
